4599a56cb5966c8c457dcb47d9d193351eac7f0e
Sri Rang <srirangan@gmail.com>
2025-03-22 14:45:40 +0100

    Sync

--- Accounts/Pathlock/gen-2025-03-19/pages/dashboard-page.ts
+++ Accounts/Pathlock/gen-2025-03-19/pages/dashboard-page.ts
@@ -5,20 +5,12 @@ export class DashboardPage {
   readonly openMenuButton: Locator;
   readonly complianceMenuItem: Locator;
   readonly campaignsMenuItem: Locator;
-  readonly adminMenuItem: Locator;
-  readonly scheduledReportsMenuItem: Locator;
-  readonly reportingMenuItem: Locator;
-  readonly scheduledReportsSubMenuItem: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.openMenuButton = page.locator('#ctl00_ctl00_div2 > ul > li:nth-child(1) > a > span.icon.anticon.icon-menuunfold');
     this.complianceMenuItem = page.locator('#ctl00_ctl00_TreeView1t148');
     this.campaignsMenuItem = page.locator('#ctl00_ctl00_TreeView1t154')
-    this.adminMenuItem = page.locator('#ctl00_ctl00_ctl00_ctl00_TreeView1t169');
-    this.scheduledReportsMenuItem = page.getByText('Scheduled Reports', { exact: true });
-    this.reportingMenuItem = page.getByText('Reporting');
-    this.scheduledReportsSubMenuItem = page.getByText('Scheduled Reports');
   }
 
   async openMenu() {
@@ -32,21 +24,5 @@ export class DashboardPage {
   async navigateToCampaigns() {
     await this.campaignsMenuItem.click();
     await this.page.waitForURL('/App/Campaigns');
-  }
-
-  async navigateToAdmin() {
-    await this.adminMenuItem.click();
-  }
-
-  async navigateToScheduledReports() {
-    await this.scheduledReportsMenuItem.click();
-  }
-
-  async navigateToReporting() {
-    await this.reportingMenuItem.click();
-  }
-
-  async navigateToScheduledReportsSubMenu() {
-    await this.scheduledReportsSubMenuItem.click();
   }
 }
--- Accounts/Pathlock/gen-2025-03-19/pages/scheduled-reports-page.ts
+++ Accounts/Pathlock/gen-2025-03-19/pages/scheduled-reports-page.ts
@@ -6,7 +6,6 @@ export class ScheduledReportsPage {
   readonly selectCheckbox: Locator;
   readonly deleteSelectedRecordsButton: Locator;
   readonly userDropdown: Locator;
-  readonly signOutButton: Locator;
 
   constructor(page: Page) {
     this.page = page;
@@ -14,7 +13,6 @@ export class ScheduledReportsPage {
     this.selectCheckbox = page.locator('[id="ctl00_ctl00_ctl00_A_DataControls_DataPlaceHolder_pageTabContainer_TabPanel1_GridView1_ctl02_selectCheckBox"]');
     this.deleteSelectedRecordsButton = page.locator('#ctl00_ctl00_ctl00_A_DataControls_DataPlaceHolder_pageTabContainer_TabPanel1_UpdatePanel3 > div > div.DeleteButtonContainer > a.enableOnRowCheck.enableOnRowCheckEnabled');
     this.userDropdown = page.locator('#ctl00_ctl00_ctl00_ProfileTailorHeader1_MenuUserProfilen0 > table > tbody > tr > td > a');
-    this.signOutButton = page.locator('#ctl00_ctl00_ctl00_ProfileTailorHeader1_MenuUserProfilen4 > td > table > tbody > tr > td > a');
   }
 
   async searchForReport(reportName: string) {
@@ -42,11 +40,5 @@ export class ScheduledReportsPage {
 
     // Wait for the URL to change after the dialog is accepted
     await this.page.waitForURL('/App/Schedules/List.aspx');
-  }
-
-  async signOut() {
-    await this.userDropdown.click();
-    await this.signOutButton.click();
-    await this.page.waitForURL('/App/ApplicationSignout.aspx');
   }
 }
