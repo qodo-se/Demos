9919beb498e9c25da4c194fbca6ae9fc858d2555
Sri Rang <srirangan@gmail.com>
2025-06-24 19:53:28 +0200

    sync

--- Sri/boring-todo/boring-todo-api/src/boring_todo_api/math/adder.py
+++ Sri/boring-todo/boring-todo-api/src/boring_todo_api/math/adder.py
@@ -12,3 +12,11 @@ def subtract_two_numbers(a, b):
 
 def subtract_three_numbers(a, b, c):
     return add_three_numbers(a, b, -c)
+
+
+def add_four_numbers(a, b, c, d):
+    return add_three_numbers(a, b, c) + d
+
+
+def add_five_numbers(a, b, c, d, e):
+    return add_four_numbers(a, b, c, d) + e
