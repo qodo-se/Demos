9d6fcfee47f574d38f77194725287ae9207db74e
Sri Rang <srirangan@gmail.com>
2025-08-19 12:15:51 +0200

    Login frontend

--- Sri/boring-todo/boring-todo-app/src/app/layout.tsx
+++ Sri/boring-todo/boring-todo-app/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { AuthProvider } from "./context/AuthContext";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -14,7 +15,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        {children}
+        <AuthProvider>
+          {children}
+        </AuthProvider>
       </body>
     </html>
   );
